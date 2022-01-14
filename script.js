f97e1ee3207154e98a0ea68b1b44bcf6b8a2d6e8
Darius Seals <tristarmediagroupinc@gmail.com>
2022-01-14 00:10:46 -0500

    contained data in Functional programming design pattern, fixed minor bug for navlink

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
-const setActive = (el) => {
+// ======Gets called on click event ==========
+
+const setActiveLink = (el) => {
     const list = document.querySelectorAll('.menu-list-items');
     list.forEach((item) =>{
-        if(item.classList.contains('.active-nav-link')){
-            item.classList.remove('.active-nav-link')
+        if(item.classList.contains('active-nav-link')){
+            item.classList.remove('active-nav-link')
         }
     })
-    el.classList.add('active')
-
+    el.classList.add('active-nav-link')
 }
 
-const arrows = document.querySelectorAll(".arrow");
-const movieLists = document.querySelectorAll(".movie-list");
-let clickCounter = 0;
-arrows.forEach((arrow,i) => {
-    
-    const itemNumber = movieLists[i].querySelectorAll("img").length;
-    arrow.addEventListener('click', () => {
-        const ratio = Math.floor(window.innerWidth / 270);
-        clickCounter++;
-        if(itemNumber - (4 + clickCounter) + (4 - ratio) >= 0){
-            movieLists[i].style.transform = `translate(${movieLists[i].computedStyleMap().get("transform")[0].x.value -300}px)`;
-        }else {
-            movieLists[i].style.transform = "translateX(0)";
-            clickCounter = 0;
-        }
+// ==========sets arrow functionality====================
+
+const setArrowFunction = () => {
+    const arrows = document.querySelectorAll(".arrow");
+    const movieLists = document.querySelectorAll(".movie-list");
+    let clickCounter = 0;
+    arrows.forEach((arrow,i) => {
         
+        const itemNumber = movieLists[i].querySelectorAll("img").length;
+        arrow.addEventListener('click', () => {
+            const ratio = Math.floor(window.innerWidth / 270);
+            clickCounter++;
+            if(itemNumber - (4 + clickCounter) + (4 - ratio) >= 0){
+                movieLists[i].style.transform = `translate(${movieLists[i].computedStyleMap().get("transform")[0].x.value -300}px)`;
+            }else {
+                movieLists[i].style.transform = "translateX(0)";
+                clickCounter = 0;
+            }
+            
+        })
+
     })
-    console.log(Math.floor(window.innerWidth / 270))
+}
 
-})
+setArrowFunction();
 
-const toggleNode = document.querySelector(".toggle-node");
-const items = document.querySelectorAll(".container, .movie-list-container, .navbar-container, .sidebar, .sidebar-icons, .toggle");
+// =============sets Toggle functionality===============
 
-toggleNode.addEventListener("click", ()=> {
-    items.forEach((item) => {
-        item.classList.toggle("active");
-    })
-    toggleNode.classList.toggle("active");
-});
+const setToogleMode = () => {
+    const toggleNode = document.querySelector(".toggle-node");
+    const items = document.querySelectorAll(".container, .movie-list-container, .navbar-container, .sidebar, .sidebar-icons, .toggle");
+
+    toggleNode.addEventListener("click", ()=> {
+        items.forEach((item) => {
+            item.classList.toggle("active");
+        })
+        toggleNode.classList.toggle("active");
+    });
+}
+
+setToogleMode();
